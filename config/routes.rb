742b310611087783575d4eed84f88b35dc4a264d
Ashutosh-kesharwani <ashutoshkesharwani20021406@gmail.com>
2025-08-01 13:31:22 +0530

    Adding Status Action in Blogs

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get 'portfolio/:id' , to: 'portfolios#show' , as: 'portfolio_show'
   get 'about-me', to: 'pages#about'
   get 'contact-us', to:'pages#contact'
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status         
+    end
+  end
   root to:'pages#home'
 end
