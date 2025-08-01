4442fbfd9c2d2096ed062e8c85976e7268700269
Ashutosh-kesharwani <ashutoshkesharwani20021406@gmail.com>
2025-08-01 17:34:47 +0530

    Implemented Validations for blogs, Portfolio, Skill

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,9 @@
 class Blog < ApplicationRecord
   enum status: {draft: 0, published: 1}
+  # Another way of writing validation
+  # validates_presence_of :title 
+  validates :title , :body , presence: true 
+
   extend FriendlyId
   friendly_id :title, use: :slugged   # or use any other field like :name
 end
