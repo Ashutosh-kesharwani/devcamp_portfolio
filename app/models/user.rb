907c0cad0f332d2abf973a5ddda524149f83816c
Ashutosh-kesharwani <ashutoshkesharwani20021406@gmail.com>
2025-08-05 12:58:16 +0530

    Completed authentication with virtual attrs for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def first_name
+    self.name.split(' ').first # these are just normal method that brings 1st ele of array
+  end
+  #Better than using self.name.split[0] because it will run only if both present if only 1 ele present for last it show error hai its direct acess indexing
+  def last_name
+    self.name.split(' ').last  # these are just normal method that brings last ele of array
+  end   
 end
